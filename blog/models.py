dca0f24d4e58a6e1a6269d8e034990258961d39a
Richard <richard.w.morton86@gmail.com>
2022-10-08 17:51:33 +0200

    end of chapter 1

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATIC_CHOICES = (
@@ -15,9 +20,22 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,choices=STATIC_CHOICES,default='draft')
+    objects = models.Manager() # the default manager
+    published = PublishedManager() # our custom manager
 
     class Meta:
         ordering = ('-publish',)
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+# def post_list(request):
+#     posts = Post.published.all()
+#     context = {'posts': posts}
+#     template = 'blog/post/list.html'
+#     return render(request,template,context)
+
+# # use paginator
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 2)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range deliver the last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,'blog/post/list.html',{'page': page,'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_detail(request,year,month,day,post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    context = {'post': post}
+    template = 'blog/post/detail.html'
+    return render(request,template,context)
