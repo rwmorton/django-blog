2d1d7f9e343caeb60123fff7fd440100c093f448
Richard <richard.w.morton86@gmail.com>
2022-10-09 08:49:21 +0200

    added tags, end chapter 2

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from .models import Post, Comment
 # from .forms import EmailPostForm
 from .forms import CommentForm
+from taggit.models import Tag
 
 # def post_list(request):
 #     posts = Post.published.all()
@@ -12,25 +13,29 @@ from .forms import CommentForm
 #     return render(request,template,context)
 
 # # use paginator
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 2)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver the last page of results
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,'blog/post/list.html',{'page': page,'posts': posts})
+def post_list(request,tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver the last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{'page': page,'posts': posts,'tag': tag})
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(
